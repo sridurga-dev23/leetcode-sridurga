3d357884ba9cf4e631f765ea58c17653f93a8759
Sri Durga R <sridurga231206@gmail.com>
2026-08-13 15:42:34 +0530

    Create 3270-MinimumMovesToCaptureTheQueen

---
+++ 3270-MinimumMovesToCaptureTheQueen/3270-MinimumMovesToCaptureTheQueen.java
@@ -0,0 +1,10 @@
+// Last updated: 8/13/2026, 3:42:33 PM
+class Solution {
+    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+    if(a == e && !(a == c && d > Math.min(b,f) && d < Math.max(b,f) )) return 1;
+    if(b == f && !(b == d && c > Math.min(a,e) && c < Math.max(a,e) ))  return 1;
+    if(c + d == e + f && !(c + d == a + b && a > Math.min(c , e) && a < Math.max(c, e) )) return 1;
+    if(c-d == e-f && !( c-d == a-b && a > Math.min(c , e) && a < Math.max(c, e) )) return 1;
+    return 2;
+}
+}
