b9ada3dc371591eccffeef9b601b1b1a28ca0a0c
Sri Durga R <sridurga231206@gmail.com>
2026-08-13 15:42:26 +0530

    Create 3330-ModifyTheMatrix

---
+++ 3330-ModifyTheMatrix/3330-ModifyTheMatrix.java
@@ -0,0 +1,26 @@
+// Last updated: 8/13/2026, 3:42:26 PM
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+      int ans[][]=new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix[0].length;i++)
+        {
+            int maxi=0;
+            for(int j=0;j<matrix.length;j++)
+            {
+                maxi=Math.max(maxi,matrix[j][i]);
+            }
+            for(int j=0;j<matrix.length;j++)
+            {
+                if(matrix[j][i]==-1)
+                {
+                    ans[j][i]=maxi;
+                }
+                else
+                {
+                    ans[j][i]=matrix[j][i];
+                }
+            }
+        }
+        return ans;  
+    }
+}
